89b29b37292e52a98bd52bd0dfc8477e9462c7b6
David J. David <david.david@doctordirectory.com>
2017-09-21 16:01:02 -0400

    Improving time.log

--- R/time.log.R
+++ R/time.log.R
@@ -9,11 +9,10 @@ time.log <- local({
                        )
   u <- "mins"
 
-
   function(action = "log", unit = NULL, comment = NULL) {
 
     if (action == "log") {
-      df.time[nrow(df.time)+1, "time"] <<- Sys.time()
+      df.time[nrow(df.time) + 1, "time"] <<- Sys.time()
       row <- nrow(df.time)
 
       if (row == 1) {
@@ -24,20 +23,13 @@ time.log <- local({
 
         if (u == unit || is.null(unit)) {
           # Calculating row.time, then total.time
-          df.time[row, 2] <<- round(difftime(df.time[row, 1], df.time[row-1, 1], units=u), 3)
-          df.time[row, 3] <<- round(df.time[row-1, 3] + df.time[row, 2], 3)
+          df.time[row, 2] <<- round(difftime(df.time[row, 1], df.time[row - 1, 1], units=u), 3)
+          df.time[row, 3] <<- round(df.time[row - 1, 3] + df.time[row, 2], 3)
         } else {
           stop("Cannot alter unit after log is created")
         }
-
-
-
       }
-
-
-
-
-
+      df.time[nrow(df.time), "comment"] <<- comment
 
     } else if (action == "return") {
       return(df.time)
@@ -48,10 +40,5 @@ time.log <- local({
     } else {
       stop("Action not recognized. See help file for list of acceptable inputs.")
     }
-
   }
 })
-
-
-
-
